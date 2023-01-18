0b0aa25a84c673d73f1c185676342c62dfdd04c0
Amanda Cleto <amanda.cleto.dev@gmail.com>
2023-01-18 14:41:38 -0300

    fix: fixing typos

--- src/graphql/modules/posts/resolvers.js
+++ src/graphql/modules/posts/resolvers.js
@@ -1,13 +1,18 @@
 import Post from "../../../models/Post";
+import User from "../../../models/User";
 
 export default {
+    Post: {
+        author: (post) => User.findById(post.author),
+    },
     Query: {
-        users: () => Post.find(),
-        user: (_, { id }) => Post.findById(id),
+        posts: () => Post.find(),
+        post: (_, { id }) => Post.findById(id),
     },
     Mutation: {
-        createPost: (_, {data}) => Post.create(data),
-        updatePost: (_, {id, data}) => Post.findOneAndUpdate(id, data, {new: true}),
-        deletePost: async (_, {id}) => !!(await Post.findOneAndDelete(id))
+        createPost: (_, { data }) => Post.create(data),
+        updatePost: (_, { id, data }) =>
+            Post.findOneAndUpdate(id, data, { new: true }),
+        deletePost: async (_, { id }) => !!(await Post.findOneAndDelete(id)),
     },
 };
