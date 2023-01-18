e6f816f618055c7265c14df1a147edb083521f4f
Amanda Cleto <amanda.cleto.dev@gmail.com>
2023-01-18 16:10:17 -0300

    fix: renaming config and env variable

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,6 @@ import * as dotenv from 'dotenv';
 dotenv.config()
 
 export default {
-    DATABASE_CONNECTION_PASSWORD: process.env.DATABASE_CONNECTION_PASSWORD,
+    DATABASE_CONNECTION_PW: process.env.DATABASE_CONNECTION_PW,
     DATABASE_CONNECTION_NAME: process.env.DATABASE_CONNECTION_NAME,
 };
--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
 import { ApolloServer } from "apollo-server";
 import mongoose from "mongoose";
-import config from './config/config';
+import config from "./config/config";
 
 function startServer({ typeDefs, resolvers }) {
-    mongoose.connect(`mongodb+srv://${config.DATABASE_CONNECTION_NAME}:${config.DATABASE_CONNECTION_PASSWORD}@cluster0.zubjunw.mongodb.net/?retryWrites=true&w=majority`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    mongoose.connect(
+        `mongodb+srv://${config.DATABASE_CONNECTION_NAME}:${config.DATABASE_CONNECTION_PW}@cluster0.rb0g9se.mongodb.net/?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    );
 
     const server = new ApolloServer({ typeDefs, resolvers });
 
     server
         .listen()
-        .then(({ url }) => console.log(`🔥 Server is ready at ${url}! `));
+        .then(({ url }) => console.log(`🔥 Server is ready at ${url}`));
 }
 
 export default startServer;
